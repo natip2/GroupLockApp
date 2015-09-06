12a8ddbf1716b36afc69285afc75b2cdde74e27d
natip2 <natip2@gmail.com>
2015-09-06 16:18:37 +0300

    init

--- MessagingTutorial/src/main/java/com/sinch/messagingtutorial/app/MyApplication.java
+++ MessagingTutorial/src/main/java/com/sinch/messagingtutorial/app/MyApplication.java
@@ -2,11 +2,12 @@ package com.sinch.messagingtutorial.app;
 
 import android.app.Application;
 import com.parse.Parse;
-
+//asjflksg
 public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        int x;
         Parse.initialize(this, "qKmz3Nbj1A3efQj8wKOnFRKafxenQJZrFuP0AOdk", "1MCwpWJH4edQMi85wcSw7LR13MhDDbzRrpaJgCBc");
     }
 }
